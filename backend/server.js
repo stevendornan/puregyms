9eeed952d06f72a1de4f465c6e05942d632912f3
stevendornan <steven.dornan93@gmail.com>
2020-12-17 18:30:07 +0000

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -44,10 +44,19 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-// Set static folder
-
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 // Sanitize data
 app.use(mongoSanitize())
